31c364f697cd09b4ea31e2877b15c7034a52badb
Jimmy Lu <jimmy.lu.2011@gmail.com>
2024-02-04 16:37:02 -0800

    add persistence

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 describe 'database' do # rubocop:disable Metrics/BlockLength
+  before do
+    `rm -rf test.db`
+  end
+
   def run_script(commands)
     raw_output = nil
-    IO.popen('./db', 'r+') do |pipe|
+    IO.popen('./db test.db', 'r+') do |pipe|
       commands.each do |command|
         pipe.puts command
       end
@@ -84,5 +88,25 @@ describe 'database' do # rubocop:disable Metrics/BlockLength
                                     'db > Executed.',
                                     'db > '
                                   ])
+  end
+
+  it 'keeps data after closing connection' do
+    result1 = run_script([
+                           'insert 1 user1 person1@example.com',
+                           '.exit'
+                         ])
+    expect(result1).to match_array([
+                                     'db > Executed.',
+                                     'db > '
+                                   ])
+    result2 = run_script([
+                           'select',
+                           '.exit'
+                         ])
+    expect(result2).to match_array([
+                                     'db > (1, user1, person1@example.com)',
+                                     'Executed.',
+                                     'db > '
+                                   ])
   end
 end
